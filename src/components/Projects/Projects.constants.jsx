9a6848bfa472e9c2b6d0cee0e5ffdf05129cd0b2
minipopstar <yumotomurai36@gmail.com>
2023-04-20 21:32:25 +0300

    Portfolio completed

--- src/components/Projects/Projects.constants.jsx
+++ src/components/Projects/Projects.constants.jsx
@@ -6,13 +6,13 @@ import getzash from '../../Assets/images/projects/getzash.png';
 import halalplates from '../../Assets/images/projects/halalplates.png';
 import getorchard from '../../Assets/images/projects/getorchard.png';
 import joinscarlett from '../../Assets/images/projects/joinscarlett.png';
-// import sundrip from '../../Assets/images/projects/sundrip.png';
-// import metamaniacsnft from '../../Assets/images/projects/metamaniacsnft.png';
-// import nftshowroom from '../../Assets/images/projects/nftshowroom.png';
-// import muskswap from '../../Assets/images/projects/muskswap.png';
-// import dexify from '../../Assets/images/projects/dexify.png';
-// import karabasannft from '../../Assets/images/projects/karabasannft.png';
-// import wallstcyborgs from '../../Assets/images/projects/wallstcyborgs.png';
+import safuu from '../../Assets/images/projects/safuu.png';
+import furio from '../../Assets/images/projects/furio.png';
+import safehavendefi from '../../Assets/images/projects/safehavendefi.png';
+import o5o from '../../Assets/images/projects/o5o.png';
+import kingflokiworld from '../../Assets/images/projects/kingflokiworld.png';
+import panwallet from '../../Assets/images/projects/panwallet.png';
+import spectrocoin from '../../Assets/images/projects/spectrocoin.png';
 
 export const web = [
 	{
@@ -61,39 +61,39 @@ export const web = [
 ];
 
 export const blockchain = [
-	// {
-	// 	name: 'Sundrip',
-	// 	cover: sundrip,
-	// 	links: [{ icon: webLink, link: 'https://dapp-prestaging.sundrip.io/' }],
-	// },
-	// {
-	// 	name: 'Metamaniacsnft',
-	// 	cover: metamaniacsnft,
-	// 	links: [{ icon: webLink, link: 'https://www.metamaniacsnft.com/' }],
-	// },
-	// {
-	// 	name: 'NFTShowroom',
-	// 	cover: nftshowroom,
-	// 	links: [{ icon: webLink, link: 'https://nftshowroom.com/' }],
-	// },
-	// {
-	// 	name: 'Muskswap',
-	// 	cover: muskswap,
-	// 	links: [{ icon: webLink, link: 'https://muskswap.dog/' }],
-	// },
-	// {
-	// 	name: 'Dexify',
-	// 	cover: dexify,
-	// 	links: [{ icon: webLink, link: 'https://www.dexify.finance/' }],
-	// },
-	// {
-	// 	name: 'Karabasannft',
-	// 	cover: karabasannft,
-	// 	links: [{ icon: webLink, link: 'https://www.karabasannft.com/' }],
-	// },
-	// {
-	// 	name: 'Wallstcyborgs',
-	// 	cover: wallstcyborgs,
-	// 	links: [{ icon: webLink, link: 'https://wallstcyborgs.com/' }],
-	// },
+	{
+		name: 'Safuu',
+		cover: safuu,
+		links: [{ icon: webLink, link: 'https://app.safuu.com/' }],
+	},
+	{
+		name: 'Furio',
+		cover: furio,
+		links: [{ icon: webLink, link: 'https://furio.io/' }],
+	},
+	{
+		name: 'Safehavendefi',
+		cover: safehavendefi,
+		links: [{ icon: webLink, link: 'https://safehavendefi.com/' }],
+	},
+	{
+		name: 'O5O',
+		cover: o5o,
+		links: [{ icon: webLink, link: 'https://mint.o5o.xyz/' }],
+	},
+	{
+		name: 'Kingflokiworld',
+		cover: kingflokiworld,
+		links: [{ icon: webLink, link: 'https://kingflokiworld.com/' }],
+	},
+	{
+		name: 'Panwallet',
+		cover: panwallet,
+		links: [{ icon: webLink, link: 'https://panwallet.io/' }],
+	},
+	{
+		name: 'Spectrocoin',
+		cover: spectrocoin,
+		links: [{ icon: webLink, link: 'https://spectrocoin.com/' }],
+	},
 ];
