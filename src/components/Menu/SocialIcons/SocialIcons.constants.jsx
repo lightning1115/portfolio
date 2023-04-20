004ca56a34d28d676cba797d7b99b16b8d83552c
bithealer <yumotomurai36@gmail.com>
2023-04-20 16:05:25 +0300

    Portfolio initialized

---
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -0,0 +1,9 @@
+// import cv from "../../../../src/Assets/pdfs/resume.pdf";
+
+export const socialLinks = [
+	{ name: 'Skype', link: 'live:.cid.afa963683093058e' },
+	{ name: 'Telegram', link: 'https://t.me/bithealer' },
+	// { name: 'Github', link: 'https://github.com/srdevhub/' },
+	{ name: 'Mail', link: 'mailto: pascalschulz1019@gmail.com' },
+	// { name: "Resume", link: cv},
+];
---
+++ src/components/Projects/Projects.constants.jsx
@@ -0,0 +1,99 @@
+import webLink from '../../Assets/images/links/globe2.svg';
+import kentmaster from '../../Assets/images/projects/kentmaster.png';
+import foliovision from '../../Assets/images/projects/foliovision.png';
+import neon from '../../Assets/images/projects/neon.png';
+import getzash from '../../Assets/images/projects/getzash.png';
+import halalplates from '../../Assets/images/projects/halalplates.png';
+import getorchard from '../../Assets/images/projects/getorchard.png';
+import joinscarlett from '../../Assets/images/projects/joinscarlett.png';
+// import sundrip from '../../Assets/images/projects/sundrip.png';
+// import metamaniacsnft from '../../Assets/images/projects/metamaniacsnft.png';
+// import nftshowroom from '../../Assets/images/projects/nftshowroom.png';
+// import muskswap from '../../Assets/images/projects/muskswap.png';
+// import dexify from '../../Assets/images/projects/dexify.png';
+// import karabasannft from '../../Assets/images/projects/karabasannft.png';
+// import wallstcyborgs from '../../Assets/images/projects/wallstcyborgs.png';
+
+export const web = [
+	{
+		name: 'Kentmaster',
+		cover: kentmaster,
+		links: [{ icon: webLink, link: 'https://kentmaster.com.au/' }],
+	},
+	{
+		name: 'Foliovision',
+		cover: foliovision,
+		links: [{ icon: webLink, link: 'https://foliovision.com/' }],
+	},
+	{
+		name: 'Neon',
+		cover: neon,
+		links: [{ icon: webLink, link: 'https://neonlink.io/' }],
+	},
+	{
+		name: 'Getzash',
+		cover: getzash,
+		links: [
+			{ icon: webLink, link: 'http://getzash.com' },
+		],
+	},
+	{
+		name: 'Halalplates',
+		cover: halalplates,
+		links: [
+			{ icon: webLink, link: 'https://halalplates.com/' },
+		],
+	},
+	{
+		name: 'Getorchard',
+		cover: getorchard,
+		links: [
+			{ icon: webLink, link: 'https://getorchard.com/' },
+		],
+	},
+	{
+		name: 'Joinscarlett',
+		cover: joinscarlett,
+		links: [
+			{ icon: webLink, link: 'https://joinscarlett.co.uk/' },
+		],
+	},
+];
+
+export const blockchain = [
+	// {
+	// 	name: 'Sundrip',
+	// 	cover: sundrip,
+	// 	links: [{ icon: webLink, link: 'https://dapp-prestaging.sundrip.io/' }],
+	// },
+	// {
+	// 	name: 'Metamaniacsnft',
+	// 	cover: metamaniacsnft,
+	// 	links: [{ icon: webLink, link: 'https://www.metamaniacsnft.com/' }],
+	// },
+	// {
+	// 	name: 'NFTShowroom',
+	// 	cover: nftshowroom,
+	// 	links: [{ icon: webLink, link: 'https://nftshowroom.com/' }],
+	// },
+	// {
+	// 	name: 'Muskswap',
+	// 	cover: muskswap,
+	// 	links: [{ icon: webLink, link: 'https://muskswap.dog/' }],
+	// },
+	// {
+	// 	name: 'Dexify',
+	// 	cover: dexify,
+	// 	links: [{ icon: webLink, link: 'https://www.dexify.finance/' }],
+	// },
+	// {
+	// 	name: 'Karabasannft',
+	// 	cover: karabasannft,
+	// 	links: [{ icon: webLink, link: 'https://www.karabasannft.com/' }],
+	// },
+	// {
+	// 	name: 'Wallstcyborgs',
+	// 	cover: wallstcyborgs,
+	// 	links: [{ icon: webLink, link: 'https://wallstcyborgs.com/' }],
+	// },
+];
